e968f64d545bbde679b53e9ab06bd0afbf1597fb
Mohamed Jasim Ahmed <30666269+MarketingLimited@users.noreply.github.com>
2025-07-25 14:46:54 +0300

    Create setup-flatpak-apps.sh

---
+++ setup-flatpak-apps.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apps=(
+    "com.bitwarden.desktop"
+    "com.adobe.Reader"
+    "com.bluemail.BlueMail"
+    "com.simplenote.Simplenote"
+    "com.blackmagicdesign.resolve"
+    "com.github.phase1geo.minder"
+)
+
+for app in "${apps[@]}"; do
+    if ! flatpak info "$app" > /dev/null 2>&1; then
+        flatpak install -y --noninteractive flathub "$app"
+    fi
+done
