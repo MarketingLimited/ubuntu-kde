d9650988ca440ee277de35b831dd3d386d4bb650
Mohamed Jasim Ahmed <30666269+MarketingLimited@users.noreply.github.com>
2025-07-27 13:39:00 +0300

    Pass environment variables to supervisord

--- entrypoint.sh
+++ entrypoint.sh
@@ -108,6 +108,8 @@ else
 fi
 
 exec env \
+    ENV_DEV_USERNAME="${DEV_USERNAME}" \
+    ENV_DEV_UID="${DEV_UID}" \
     DEV_USERNAME="${DEV_USERNAME}" DEV_UID="${DEV_UID}" \
     XDG_RUNTIME_DIR="/run/user/${DEV_UID}" \
     /usr/bin/supervisord -c /etc/supervisor/supervisord.conf -n
